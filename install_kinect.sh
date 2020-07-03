ebbaa7d74a298524fd5b55320dfb9c2b9292b22e
root <justin@diyautospec.com>
2020-07-02 19:48:30 -0600

    write install script

---
+++ install_kinect.sh
@@ -0,0 +1,20 @@
+#install lib_usb
+apt-get install libudev-dev -y
+wget -O libusb.tar.bz2 https://github.com/libusb/libusb/releases/download/v1.0.23/libusb-1.0.23.tar.bz2
+tar xvjf libusb.tar.bz2
+cd libusb-1.0.23
+./configure
+make
+sudo make install
+cd ..
+
+#install libfreenect
+apt-get install cmake cmake-curses-gui pkg-config
+
+git clone https://github.com/OpenKinect/libfreenect.git
+cd libfreenect
+mkdir build
+cd build
+cmake ..
+make
+make install
